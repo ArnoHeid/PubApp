2b400867e23390f2ec2641b424d5193f45b79a36
arnoheid <arno@iheidelberg.com>
2016-12-03 10:42:52 +0100

    Added geocoder Main / Resource Changed geocoder pom.xml
    Hello World done

---
+++ geocoder/src/main/java/de/hs_mainz/pubApp/Resource.java
@@ -0,0 +1,25 @@
+package de.hs_mainz.pubApp;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Arno on 03.12.2016.
+ */
+@Path("geocoder")
+public class Resource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
