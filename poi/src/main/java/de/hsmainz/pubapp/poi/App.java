9feb4bf9af96c364d30a74d067af25567850dd3c
Carolin Rottmann <rottmann.carolin@gmail.com>
2016-12-09 19:32:23 +0100

    - Verbindung mit Google API funktioniert. - JSON wird entsprechend der POI Auswahl und Koordinaten zurückgegeben. - Noch keine Speicherung als Place oder weitere Verarbeitung - API Key ist ****, richtiger Key liegt lokal vor

--- poi/src/main/java/de/hsmainz/pubapp/poi/App.java
+++ poi/src/main/java/de/hsmainz/pubapp/poi/App.java
@@ -1,13 +1,39 @@
 package de.hsmainz.pubapp.poi;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import de.hsmainz.pubapp.poi.controller.PoiService;
 /**
  * Hello world!
  *
  */
+@SuppressWarnings("restriction")
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        PoiService poiService = new PoiService();
+        PoiService.search("bar", 49.995123, 8.267426, 4);
+        
     }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
 }
