cede09266e7ba97f05d3f92890515c98a7b48149
Lorenz <Lorenz@Donger>
2016-12-21 16:00:35 +0100

    All Microservices now function correctly.

--- Client/leaflet/main.js
+++ Client/leaflet/main.js
@@ -1,6 +1,10 @@
 var mymap;
 var GEOJSON;
-
+var api_geocoder = "http://localhost:8080/pubapp/geocoder";	/* URL für den Microservice: Geocoder  */
+var api_routing = "http://localhost:8090/pubapp/routing";	/* URL für den Microservice: Routing  */
+var api_poi = "http://localhost:8000/poi";		/* URL für den Microservice: Points of Interest  */
+var routing_arr;
+var geocoder_json;
 function init() {																// Sobald das HTML Dokument geladen wird, wird eine Karte erstellt //
 	 mymap = L.map('mapid').setView([50, 8.26], 14);
 	
@@ -16,8 +20,7 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
 	}
 
 	mymap.on('click', onMapClick);
-	
-	 GEOJSON = L.geoJSON();	/*Erstellt einen Layer für die Darstellung von GEOJSON Daten*/		
+		
 	
 }
 	
@@ -25,14 +28,12 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
 	
 	
 JSONTest = function() {
-	
-	var api_geocoder = "http://localhost:8080/pubapp/geocoder";	/* URL für den Microservice: Geocoder  */
-	/*var api2 = "http://localhost:8080/pubapp/routing";	/* URL für den Microservice: Routing  */
-	/*var api3 = "http://localhost:8080/pubapp/poi";		/* URL für den Microservice: Points of Interest  */
+	if (GEOJSON != null) {
+		GEOJSON.remove();
+	}
 	
 	var Startpunkt = document.getElementById("startpunkt").value;	/* Speichert die Eingabe aus dem Feld "Startpunkt" */
 	var Endpunkt = document.getElementById("endpunkt").value;		/* Speichert die Eingabe aus dem Feld "Startpunkt" */
-	/* alert("Startpunkt: " + Startpunkt + "Endpunkt: " + Endpunkt); */
 	document.getElementById("startpunkt").value = null;				/* Setz den Wert der Textfelder auf null */
 	document.getElementById("endpunkt").value = null;				/*	 Setz den Wert der Textfelder auf null */
 	
@@ -40,18 +41,71 @@ JSONTest = function() {
     type: 'GET',
     dataType: 'jsonp',
     url: api_geocoder,
-	data: 'queryString=Wiesbaden&locale=de',	/* Schickt die Eingaben als jQuery an den Microservice */
+	data: 'queryString=' + Startpunkt + '&locale=de',	/* Schickt die Eingaben als jQuery an den Microservice */
+    crossDomain : true,
+    xhrFields: { withCredentials: true },
+	success: 
+	function (data) {
+		console.log(data);
+		GEOJSON = L.geoJSON(data).addTo(mymap);
+		}
+	})
+    .done(function( data ) {
+        console.log("done");
+    })
+    .fail( function(xhr, textStatus, errorThrown) {
+        console.log(xhr);
+        alert(textStatus);
+    });
+	console.log(geocoder_json);
+}
+
+routing = function() {
+	if (GEOJSON != null) {
+		GEOJSON.remove();
+	}
+	
+	$.ajax({														/* Zweite Ajax-Abfrage mit GET */
+    type: 'GET',
+    dataType: 'jsonp',
+    url: api_routing,
+	data: 'startPoint=50.938056,6.956944&endPoint=50,8.271111',	/* Schickt die Eingaben als jQuery an den Microservice */
+	//data: 'startPoint=49.94567,8.25485&endPoint=50.01279,8.28472&locale=de&pointsEncoded=false',
+    crossDomain : true,
+    xhrFields: { withCredentials: true },
+	success: 
+	function (data) {
+		var polyline = L.geoJSON(data, {color: 'red'}).addTo(mymap);
+		}
+	})
+    .done(function( data ) {
+        console.log("done");
+    })
+    .fail( function(xhr, textStatus, errorThrown) {
+        console.log(xhr);
+        alert(textStatus);
+    });
+}
+
+var test_GJSON = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"REWE"},"geometry":{"type":"Point","coordinates":[8.3146131,49.9643788]}},{"type":"Feature","properties":{"name":"Nahkauf"},"geometry":{"type":"Point","coordinates":[8.32054,49.96446]}},{"type":"Feature","properties":{"name":"ADM Mainz GmbH"},"geometry":{"type":"Point","coordinates":[8.315769999999999,49.97296]}},{"type":"Feature","properties":{"name":"DHL Packstation"},"geometry":{"type":"Point","coordinates":[8.31864,49.9606]}}]};
+POI = function() {
+	GEOJSON = L.geoJSON(test_GJSON).addTo(mymap);
+}
+/*POI = function() {
+	if (GEOJSON != null) {
+		GEOJSON.remove();
+	}				
+	
+	$.ajax({														
+    type: 'GET',
+    dataType: 'jsonp',
+    url: api_poi,
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: function (data) {
-		L.geoJSON(data, {
-		style: function (feature) {
-        return {color: feature.properties.color};
+		console.log(data);
+		GEOJSON = L.geoJSON(data).addTo(mymap);
 		}
-		}).bindPopup(function (layer) {
-			return layer.feature.properties.description;
-		}).addTo(mymap);
-	}
 	})
     .done(function( data ) {
         console.log("done");
@@ -60,43 +114,4 @@ JSONTest = function() {
         console.log(xhr);
         alert(textStatus);
     });
-
-	/*$.ajax({														 Zweite Ajax-Abfrage mit GET 
-    type: "GET",
-    dataType: 'jsonp',
-    url: api2,
-	data: 'queryText={"queryString" : "Wiesbaden","locale": "de"}',
-    crossDomain : true,
-    xhrFields: { withCredentials: true },
-	success: function(data) {
-		console.log(data);
-	}
-	})
-    .done(function( data ) {
-        console.log("done");
-    })
-    .fail( function(xhr, textStatus, errorThrown) {
-        console.log(xhr);
-        alert(textStatus);
-    });
-
-	$.ajax({														/* Dritte Ajax-Abfrage mit GET 
-    type: "GET",
-    dataType: 'jsonp',
-    url: api3,
-	data: 'queryText={"queryString" : "Wiesbaden","locale": "de"}',
-    crossDomain : true,
-    xhrFields: { withCredentials: true },
-	success: function(data) {
-		console.log(data);
-	}
-	})
-    .done(function( data ) {
-        console.log("done");
-    })
-    .fail( function(xhr, textStatus, errorThrown) {
-        console.log(xhr);
-        alert(textStatus);
-    }); */
-	
-}
+}*/
