c126eb64bb1e6bcbb88c6763da3d941348e1c8c4
SarahKoziol <sarah.koziol1991@gmail.com>
2016-12-21 11:03:35 +0100

    Ein Kommentar und Anlegen von "Test"

--- routing/src/main/java/de/hsmainz/pubApp/routing/jsonparser/geoJson/Properties.java
+++ routing/src/main/java/de/hsmainz/pubApp/routing/jsonparser/geoJson/Properties.java
@@ -3,5 +3,5 @@ package de.hsmainz.pubApp.routing.jsonparser.geoJson;
 /**
  * Created by Sarah on 20.12.2016.
  */
-public class Properties {
+public class Properties {           //Do I need Properties? Is it useful in this case?
 }
