6917729fbc6bd27360795bb98a1f17642eaf504a
Lorenz <Lorenz@Donger>
2017-02-02 20:11:18 +0100

    Microservice for POI now works with user input

--- client/leaflet/main.js
+++ client/leaflet/main.js
@@ -2,13 +2,22 @@ var mymap;
 var GEOJSON;
 var api_geocoder = "http://localhost:8080/pubapp/geocoder";	/* URL für den Microservice: Geocoder  */
 var api_routing = "http://localhost:8090/pubapp/routing";	/* URL für den Microservice: Routing  */
-var api_poi = "http://localhost:8000/pupapp/poi";		/* URL für den Microservice: Points of Interest  */
+var api_poi = "http://localhost:8000/pubapp/poi";		/* URL für den Microservice: Points of Interest  */
 var routing_arr;
 var geocoder_json;
 var lat;
 var longitude;
 var polyline;
 var i = 0;
+var Startpunkt;
+var Endpunkt;
+var startclicklat;
+var startclicklng;
+var endclicklat;
+var endclicklng;
+var place;
+var interest = new Array();
+var intereststring;
 function init() {																// Sobald das HTML Dokument geladen wird, wird eine Karte erstellt //
 	 mymap = L.map('mapid').setView([50, 8.26], 14);
 	
@@ -20,11 +29,23 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);	
+        .openOn(mymap);
+		if (i == 0) {
+		startclicklat = e.latlng.lat;
+		startclicklng = e.latlng.lng;
+		$('#startpunkt_button').val(startclicklat + ", " + startclicklng);
+		i = i + 1;
+		}
+		else {
+		endclicklat = e.latlng.lat;
+		endclicklng = e.latlng.lng;
+		$('#endpunkt_button').val(endclicklat + ", " + endclicklng);
+		i = 0; }
 	}
 
 	mymap.on('click', onMapClick);
-		
+	
+	$('#Startpunkt').hide();	
 	$('#Endpunkt').hide();
 	$('#notification').hide();
 	$('#routing').hide();
@@ -33,40 +54,46 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
 
 function onEachFeature(feature, layer) {
 	layer.on('click', function(e) {
-		console.log(i);
-		var lat = e.latlng.lat;
-		var lng = e.latlng.lng;
 		if (i == 0) {
-		$('#startpunkt_button').val(lat + ", " + lng);
+		startclicklat = e.latlng.lat;
+		startclicklng = e.latlng.lng;
+		$('#startpunkt_button').val(startclicklat + ", " + startclicklng);
 		i = i + 1;
-		console.log(i);	}
-		else {
-		$('#endpunkt_button').val(lat + ", " + lng);
-		i = 0; }
 		console.log(e);
+		}
+		else {
+		endclicklat = e.latlng.lat;
+		endclicklng = e.latlng.lng;
+		$('#endpunkt_button').val(endclicklat + ", " + endclicklng);
+		i = 0;
+		console.log(e);
+		}
 	});
-}		
+}
+
+
+
+		
 
 jQuery.support.cors = true;
 	
-JSONTest = function() {
+geocoder = function() {
 	if (GEOJSON != null) {
 		GEOJSON.remove();
 	}
 	
-	if($('#startpunkt').val() == "") {
+	if($('#search_button').val() == "") {
 		alert("Sie müssen einen Startpunkt angeben!");
 	}
 	else {
-	var Startpunkt = document.getElementById("startpunkt_button").value;	/* Speichert die Eingabe aus dem Feld "Startpunkt" */
-	var Endpunkt = document.getElementById("endpunkt_button").value;		/* Speichert die Eingabe aus dem Feld "Startpunkt" */
+	place = document.getElementById("search_button").value;	
 	document.getElementById("startpunkt_button").value = null;				/* Setz den Wert der Textfelder auf null */
 	document.getElementById("endpunkt_button").value = null;				/*	 Setz den Wert der Textfelder auf null */
 	
 	$.post({														/* Erste Ajax-Abfrage mit GET */
     dataType: 'jsonp',
     url: api_geocoder,
-	data: 'queryString=' + Startpunkt + '&locale=de',	/* Schickt die Eingaben als jQuery an den Microservice */
+	data: 'queryString=' + place + '&locale=de',	/* Schickt die Eingaben als jQuery an den Microservice */
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: 
@@ -75,10 +102,11 @@ JSONTest = function() {
 	GEOJSON = L.geoJSON(data, {
 		onEachFeature: onEachFeature
 	}).addTo(mymap);
+		$('#Startpunkt').show();
 		$('#Endpunkt').show();
 		$('#notification').html("<b>Bitte wählen Sie von der Karte einen Start- und Endpunkt aus!</b>").slideDown();
 		$('#routing').show();
-	$('#poi').show();
+		$('#poi').show();
 		}
 	})
     .done(function( data ) {
@@ -95,6 +123,9 @@ JSONTest = function() {
 routing = function() {
 	if (GEOJSON != null) {
 		GEOJSON.remove();
+	}
+	if (polyline != null) {
+		polyline.remove();
 	}
 	var Startpunkt = document.getElementById("startpunkt_button").value;
 	var Endpunkt = document.getElementById("endpunkt_button").value;	
@@ -119,24 +150,30 @@ routing = function() {
     });
 }
 
-var test_GJSON = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"REWE"},"geometry":{"type":"Point","coordinates":[8.3146131,49.9643788]}},{"type":"Feature","properties":{"name":"Nahkauf"},"geometry":{"type":"Point","coordinates":[8.32054,49.96446]}},{"type":"Feature","properties":{"name":"ADM Mainz GmbH"},"geometry":{"type":"Point","coordinates":[8.315769999999999,49.97296]}},{"type":"Feature","properties":{"name":"DHL Packstation"},"geometry":{"type":"Point","coordinates":[8.31864,49.9606]}}]};
+
 POI = function() {
-	GEOJSON = L.geoJSON(test_GJSON).addTo(mymap);
-}
-/*POI = function() {
 	if (GEOJSON != null) {
 		GEOJSON.remove();
-	}				
-	
+	}
+	for (c = 0; c < document.forms[0].length; c++) {
+		if (document.forms[0].elements[c].checked) {
+			interest[c] = document.getElementById('interest' + (c+1)).value;
+		}
+	}
 	$.ajax({														
     type: 'GET',
     dataType: 'jsonp',
     url: api_poi,
+	data: 'criteria={"interests":["' + interest[0] + '","atm"],"coordinates":[{"lat":' + startclicklat + ',"lng":' + startclicklng + '},{"lat":' + endclicklat + ',"lng":' + endclicklng + '}]}',
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: function (data) {
 		console.log(data);
-		GEOJSON = L.geoJSON(data).addTo(mymap);
+		GEOJSON = L.geoJSON(data, {
+		onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.name);
+		}
+		}).addTo(mymap);
 		}
 	})
     .done(function( data ) {
@@ -146,4 +183,23 @@ POI = function() {
         console.log(xhr);
         alert(textStatus);
     });
-}*/
+}
+
+clearall = function() {
+		console.log("clear");
+		if(GEOJSON != null) {
+		GEOJSON.remove();
+		polyline.remove(); }
+}
+
+selectall = function() {
+	for (c = 0; c < document.forms[0].length; c++) {
+		document.forms[0].elements[c].checked = true;
+	}
+}
+
+deselectall = function() {
+	for (c = 0; c < document.forms[0].length; c++) {
+		document.forms[0].elements[c].checked = false;
+	}
+}
