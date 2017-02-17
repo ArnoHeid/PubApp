8c5c11a047ff2666a86b7e2c506572229e603f31
Lorenz <Lorenz@Donger>
2017-02-17 10:47:10 +0100

    Webseite now uses normalize.css POI with BBoX now functions correctly Startpoint will be remembered now

--- client/leaflet/main.js
+++ client/leaflet/main.js
@@ -1,8 +1,8 @@
 var mymap;
 var GEOJSON;
-var api_geocoder = "http://localhost:8080/pubapp/geocoder";	/* URL für den Microservice: Geocoder  */
-var api_routing = "http://localhost:8090/pubapp/routing";	/* URL für den Microservice: Routing  */
-var api_poi = "http://localhost:8000/pubapp/poi";		/* URL für den Microservice: Points of Interest  */
+var api_geocoder = "http://143.93.114.139/geocoder";	/* URL für den Microservice: Geocoder  */
+var api_routing = "http://143.93.114.139/routing";	/* URL für den Microservice: Routing  */
+var api_poi = "http://143.93.114.139/poi";		/* URL für den Microservice: Points of Interest  */
 var routing_arr;
 var geocoder_json;
 var lat;
@@ -20,13 +20,23 @@ var interest = new Array();
 var intereststring;
 var layer_marker1;
 var layer_marker2;
+var tmpStart;
+var tmpEnde;
+var myJsonString;
 function init() {																// Sobald das HTML Dokument geladen wird, wird eine Karte erstellt //
 	 mymap = L.map('mapid').setView([50, 8.26], 14);
 	
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	/*L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: 'Map by Leaflet; Ein Studentenprojekt der Hochschule Mainz: <a href=information.html>Information</a>'
-	}).addTo(mymap);
+	}).addTo(mymap);*/
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>, Powered by Google and Overpass',
+            maxZoom: 18,
+            id: 'mapbox.light',
+            accessToken: 'pk.eyJ1IjoibG9yb2RvbiIsImEiOiJjaXo5a25uMzEwMDFlMzNvMHRrN3Rpd251In0.rbyeEiTl7Rzr7R-YlxwzSQ'
+        }).addTo(mymap);
 	
+
 	var popup = L.popup();
 	
 	function onMapClick(e) {
@@ -42,6 +52,7 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
 		startclicklat = e.latlng.lat;
 		startclicklng = e.latlng.lng;
 		$('#startpunkt_button').val(startclicklat + ", " + startclicklng);
+		tmpStart = startclicklat + ", " + startclicklng;
 		i = i + 1;
 		}
 		else {
@@ -52,6 +63,7 @@ function init() {																// Sobald das HTML Dokument geladen wird, wird 
 		endclicklat = e.latlng.lat;
 		endclicklng = e.latlng.lng;
 		$('#endpunkt_button').val(endclicklat + ", " + endclicklng);
+		tmpEnde = endclicklat + ", " + endclicklng;
 		i = 0; }
 	}
 
@@ -69,6 +81,7 @@ function getCoords(feature, layer) {
 		startclicklat = e.latlng.lat;
 		startclicklng = e.latlng.lng;
 		$('#startpunkt_button').val(startclicklat + ", " + startclicklng);
+				tmpStart = startclicklat + ", " + startclicklng;
 		i = i + 1;
 		$('#notification').html("<b>Bitte wählen Sie jetzt von der Karte einen Endpunkt aus!</b>");
 		}
@@ -76,6 +89,7 @@ function getCoords(feature, layer) {
 		endclicklat = e.latlng.lat;
 		endclicklng = e.latlng.lng;
 		$('#endpunkt_button').val(endclicklat + ", " + endclicklng);
+				tmpEnde = endclicklat + ", " + endclicklng;
 		i = 0;
 		console.log(e);
 		}
@@ -83,10 +97,16 @@ function getCoords(feature, layer) {
 }
 
 clearall = function() {
-		console.log("clear");
-		if(GEOJSON != null) {
-		GEOJSON.remove();
-		polyline.remove(); }
+		mymap.remove();
+		mymap = L.map('mapid').setView([50, 8.26], 14);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>, Powered by Google and Overpass',
+            maxZoom: 18,
+            id: 'mapbox.light',
+            accessToken: 'pk.eyJ1IjoibG9yb2RvbiIsImEiOiJjaXo5a25uMzEwMDFlMzNvMHRrN3Rpd251In0.rbyeEiTl7Rzr7R-YlxwzSQ'
+        }).addTo(mymap);
+	$('#endpunkt_button').val(null);
+	$('#startpunkt_button').val(null);
 }
 
 selectall = function() {
@@ -128,7 +148,6 @@ geocoder = function() {
 	place = document.getElementById("search_button").value;	
 	document.getElementById("startpunkt_button").value = null;				/* Setz den Wert der Textfelder auf null */
 	document.getElementById("endpunkt_button").value = null;				/*	 Setz den Wert der Textfelder auf null */
-	
 	$.post({														/* Erste Ajax-Abfrage mit GET */
     dataType: 'jsonp',
     url: api_geocoder,
@@ -137,7 +156,11 @@ geocoder = function() {
     xhrFields: { withCredentials: true },
 	success: 
 	function (data) {
-	console.log(data);
+	tmpStart = data.features[0].geometry.coordinates[1] + ',' + data.features[0].geometry.coordinates[0];
+	startclicklat = data.features[0].geometry.coordinates[1];
+	startclicklng = data.features[0].geometry.coordinates[0];
+	document.getElementById("startpunkt_button").value = data.features[0].geometry.coordinates[1] + ',' + data.features[0].geometry.coordinates[0];
+	i = i + 1;
 	GEOJSON = L.geoJSON(data, {
 		onEachFeature: getCoords
 	}).addTo(mymap);
@@ -164,15 +187,14 @@ routing = function() {
 	if (polyline != null) {
 		polyline.remove();
 	}
-	var Startpunkt = document.getElementById("startpunkt_button").value;
-	var Endpunkt = document.getElementById("endpunkt_button").value;
-	console.log(Startpunkt);
-	console.log(Endpunkt);
+	console.log(tmpStart);
+	tmpEnde = document.getElementById("endpunkt_button").value;
+	console.log(tmpEnde);
 	$.ajax({														/* Zweite Ajax-Abfrage mit GET */
     type: 'GET',
     dataType: 'jsonp',
     url: api_routing,
-	data: 'startPoint=' + Startpunkt + '&endPoint=' + Endpunkt,	/* Schickt die Eingaben als jQuery an den Microservice */
+	data: 'startPoint=' + tmpStart + '&endPoint=' + tmpEnde,	/* Schickt die Eingaben als jQuery an den Microservice */
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: 
@@ -207,14 +229,16 @@ POI_BBX = function() {
 	for (c = 0; c < document.forms[0].length; c++) {
 		
 		if (document.forms[0].elements[c].checked) {
-			interest[c] = document.getElementById('interest' + (c+1)).value;
+			interest.push(document.getElementById('interest' + (c+1)).value);
 		}
 	}
+	console.log(interest);
+	myJsonString = JSON.stringify(interest);
 	$.ajax({														
     type: 'GET',
     dataType: 'jsonp',
     url: api_poi,
-	data: 'criteria={"interests":["' + interest[0] + '","' + interest[1] + '","' + interest[2] + '","' + interest[3] + '","' + interest[4] + '","' + interest[5] + '"],"coordinates":[{"lat":' + polyline.getBounds().getSouthWest().lat + ',"lng":' + polyline.getBounds().getSouthWest().lng + '},{"lat":' + polyline.getBounds().getNorthEast().lat + ',"lng":' + polyline.getBounds().getNorthEast().lng + '}]}&searchtype=bbox',
+	data: 'criteria={"interests":' + myJsonString + ',"coordinates":[{"lat":' + polyline.getBounds().getSouthWest().lat + ',"lng":' + polyline.getBounds().getSouthWest().lng + '},{"lat":' + polyline.getBounds().getNorthEast().lat + ',"lng":' + polyline.getBounds().getNorthEast().lng + '}]}&searchtype=bbox',
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: function (data) {
@@ -224,7 +248,7 @@ POI_BBX = function() {
         layer.bindPopup('<div class="popup">' + feature.properties.interest + '<br>' + feature.properties.name + '<p>' + feature.properties.openingHours + '<p></div>');
 		},
 		pointToLayer: function(feature, latlng) {
-			if (feature.properties.interest == 'Bar') {
+			if (feature.properties.interest == 'bar') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/beer.png',
 					iconAnchor: [13, 27],
@@ -232,7 +256,7 @@ POI_BBX = function() {
 				});
 				return L.marker(latlng, {icon: barIcon});
 				}
-			if (feature.properties.interest == 'Restaurant') {
+			if (feature.properties.interest == 'restaurant') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/restaurant.png',
 					iconAnchor: [13, 27],
@@ -290,14 +314,15 @@ POI = function() {
 	for (c = 0; c < document.forms[0].length; c++) {
 		
 		if (document.forms[0].elements[c].checked) {
-			interest[c] = document.getElementById('interest' + (c+1)).value;
+			interest.push(document.getElementById('interest' + (c+1)).value);
 		}
 	}
+	myJsonString = JSON.stringify(interest);
 	$.ajax({														
     type: 'GET',
     dataType: 'jsonp',
     url: api_poi,
-	data: 'criteria={"interests":["' + interest[0] + '","' + interest[1] + '","' + interest[2] + '","' + interest[3] + '","' + interest[4] + '","' + interest[5] + '"],"coordinates":[{"lat":' + startclicklat + ',"lng":' + startclicklng + '},{"lat":' + endclicklat + ',"lng":' + endclicklng + '}]}',
+	data: 'criteria={"interests":' + myJsonString + ',"coordinates":[{"lat":' + startclicklat + ',"lng":' + startclicklng + '},{"lat":' + endclicklat + ',"lng":' + endclicklng + '}]}',
     crossDomain : true,
     xhrFields: { withCredentials: true },
 	success: function (data) {
@@ -307,7 +332,7 @@ POI = function() {
 			layer.bindPopup('<div class="popup">'  + feature.properties.interest + '<br>' + feature.properties.name + '<p>' + feature.properties.openingHours + '<p></div>');
 			},
 		pointToLayer: function(feature, latlng) {
-			if (feature.properties.interest == 'Bar') {
+			if (feature.properties.interest == 'bar') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/beer.png',
 					iconAnchor: [13, 27],
@@ -315,7 +340,7 @@ POI = function() {
 				});
 				return L.marker(latlng, {icon: barIcon});
 				}
-			if (feature.properties.interest == 'Restaurant') {
+			if (feature.properties.interest == 'restaurant') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/restaurant.png',
 					iconAnchor: [13, 27],
@@ -323,7 +348,7 @@ POI = function() {
 				});
 				return L.marker(latlng, {icon: barIcon});
 				}
-			if (feature.properties.interest == 'ATM') {
+			if (feature.properties.interest == 'atm') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/atm.png',
 					iconAnchor: [13, 27],
@@ -331,7 +356,7 @@ POI = function() {
 				});
 				return L.marker(latlng, {icon: barIcon});
 				}
-			if (feature.properties.interest == 'Nachtclub') {
+			if (feature.properties.interest == 'night_club') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/club.png',
 					iconAnchor: [13, 27],
@@ -339,7 +364,7 @@ POI = function() {
 				});
 				return L.marker(latlng, {icon: barIcon});
 				}
-			if (feature.properties.interest == 'Cafe') {
+			if (feature.properties.interest == 'cafe') {
 				var barIcon = L.icon({
 					iconUrl: './leaflet/png/cafe.png',
 					iconAnchor: [13, 27],
@@ -359,4 +384,3 @@ POI = function() {
         alert(textStatus);
     });
 }
-
